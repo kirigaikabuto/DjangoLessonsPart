9ba0d94835a33ae7bd93c9dd753f94014d73f43c
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-06-22 19:50:32 +0600

    changes

--- Part2/mysite/views.py
+++ Part2/mysite/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def main_page(request):
     print("it is main page")
-    return HttpResponse("it is main page")
+    return render(request, "main/main_page.html")
 
 
 def contacts_page(request):
     print("it is contact page")
-    return HttpResponse("it is contact page")
+    return render(request,"main/contacts_page.html")
 
 
 def about_us_page(request):
