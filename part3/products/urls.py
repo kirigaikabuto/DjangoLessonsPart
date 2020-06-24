87034e7a3784ab64a3d7805086442cf229d77e90
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-06-24 20:05:58 +0600

    part3

--- part3/products/urls.py
+++ part3/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path("all/",list_products),
-    path("detail/<str:name>/",detail)
+    path("detail/<str:name>/",detail),
 ]
+# http://127.0.0.1:8000/products/detail/sdsdsd/
--- part3/products/views.py
+++ part3/products/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 
 def list_products(request):
+
     return render(request, "products/list_page.html")
 
 
-def detail(request,name):
-    return render(request,"products/detail_page.html")
+def detail(request, name):
+    product = Product.objects.get(name=name)
+    d = {
+        "product" : product,
+    }
+    return render(request, "products/detail_page.html", context=d)
