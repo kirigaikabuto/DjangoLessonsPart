c6fe60682e2624c7fbb56cdcab2698c4c5897986
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-07-22 12:55:11 +0600

    sdsd

--- NewUserProfile/orders/views.py
+++ NewUserProfile/orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def order(request):
+    product_id = request.POST['product_id']
+    quantuty = request.POST['quantity']
+    product = Product.objects.get(pk=product_id)
+    if product.quantity<quantuty:
+        return HttpRessponse("Унас нет такого колво")
