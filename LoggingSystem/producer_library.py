af50ee979c810f19fcd5711d001d933bd398345b
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-07-27 19:49:56 +0600

    sdsd

---
+++ LoggingSystem/producer_library.py
@@ -0,0 +1,11 @@
+from kafka import KafkaProducer
+import json
+
+
+def send_log(log):
+    bootstrap_servers = ['localhost:9092']
+    topic_name = 'new_topic'
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+    data_json = json.dumps(log)
+    producer.send(topic_name, bytearray(data_json, 'utf-8'))
+    producer.flush()
